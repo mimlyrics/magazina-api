0208fcb6935aff4a8b0ad0ba13a96d7be37945b7
mimlyrics <mimlyrics8@gmail.com>
2025-01-03 13:05:21 +0100

    latest 3DEC 13:05

--- src/main/java/com/magazin/magazina/auth/AuthenticationController.java
+++ src/main/java/com/magazin/magazina/auth/AuthenticationController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import jakarta.servlet.http.HttpServletResponse;
 
 @CrossOrigin(
-        origins = "http://localhost:3000", // Frontend URL
+        origins = "https://magazina.onrender.com", // Frontend URL
         allowedHeaders = "*", // Allow all headers
         methods = {RequestMethod.POST, RequestMethod.GET, RequestMethod.OPTIONS}, // Allowed methods
         allowCredentials = "true" // Allow credentials like cookies
