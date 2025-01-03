36dc8caa8a096e742c4beed94acd9ab6fb726b82
mimlyrics <154952462+mimlyrics@users.noreply.github.com>
2025-01-03 14:32:30 +0100

    Update UserController.java

--- src/main/java/com/magazin/magazina/controllers/UserController.java
+++ src/main/java/com/magazin/magazina/controllers/UserController.java
@@ -6,7 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@CrossOrigin(
+        origins = "https://magazina.onrender.com", // Frontend URL
+        allowedHeaders = "*", // Allow all headers
+        methods = {RequestMethod.POST, RequestMethod.GET, RequestMethod.OPTIONS}, // Allowed methods
+        allowCredentials = "true" // Allow credentials like cookies
+)
 @RestController
 @RequestMapping("/api/v1/users")
 public class UserController {
