51fc21e4bba0bfcbc2eeea704cba526359472470
mimlyrics <154952462+mimlyrics@users.noreply.github.com>
2025-01-03 14:36:20 +0100

    Update SupplierController.java

--- src/main/java/com/magazin/magazina/controllers/SupplierController.java
+++ src/main/java/com/magazin/magazina/controllers/SupplierController.java
@@ -15,7 +15,12 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
-
+@CrossOrigin(
+        origins = "https://magazina.onrender.com", // Frontend URL
+        allowedHeaders = "*", // Allow all headers
+        methods = {RequestMethod.POST, RequestMethod.GET, RequestMethod.OPTIONS}, // Allowed methods
+        allowCredentials = "true" // Allow credentials like cookies
+)
 @RestController
 @RequestMapping("/api/v1/suppliers")
 public class SupplierController {
