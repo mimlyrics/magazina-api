6e1753152e1bbee12df90c4b07c16d636daafc3c
mimlyrics <154952462+mimlyrics@users.noreply.github.com>
2025-01-03 14:34:34 +0100

    Update StatusController.java

--- src/main/java/com/magazin/magazina/controllers/StatusController.java
+++ src/main/java/com/magazin/magazina/controllers/StatusController.java
@@ -9,7 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-
+@CrossOrigin(
+        origins = "https://magazina.onrender.com", // Frontend URL
+        allowedHeaders = "*", // Allow all headers
+        methods = {RequestMethod.POST, RequestMethod.GET, RequestMethod.OPTIONS}, // Allowed methods
+        allowCredentials = "true" // Allow credentials like cookies
+)
 @RestController
 @RequestMapping("/api/v1/statuses")
 public class StatusController {
